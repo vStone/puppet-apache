d093e321bfc7d2b191bb3c7574001b6ecff6f4bb
Dave Simons <dave@inuits.be>
2011-07-15 09:16:11 +0200

    add centos6 compat
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,9 +6,15 @@ class apache::config {
 		mode => 0644,
 		name => $operatingsystem ? {
 			/Debian|Ubuntu/ => '/etc/apache2/apache2.conf',
-			/Centos|Fedora/ => '/etc/httpd/conf/httpd.conf',
+			Centos => '/etc/httpd/conf/httpd.conf',
 		},
-		content => template('apache/httpd.conf'),
+		content => $operatingsystem ? {
+			'Centos' => $operatingsystemrelease ? {
+				'6.0' => template('apache/apache.conf-centos6.erb'),
+				'*' => template('apache/apache.conf'),
+			},
+			'*' => template('apache/apache.conf'),
+		},
 		notify => Service['apache'],
 	}
 }

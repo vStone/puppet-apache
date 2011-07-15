42c631e1d9b7ec9cf594cf9e4eda27a10cea0a10
Dave Simons <dave@inuits.be>
2011-07-15 09:16:29 +0200

    add encompassing apache class
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,3 +5,11 @@ import 'mod/passenger.pp'
 import 'mod/prefork.pp'
 import 'mod/php.pp'
 import 'mod/xsendfile.pp'
+
+class apache {
+	include apache::packages
+	include apache::config
+	include apache::service
+
+	Class['apache::packages'] -> Class['apache::config'] -> Class['apache::service']
+}

42dbd681ef9cb00a070e71b1a7b931c4a303dd2c
Dave Simons <dave@inuits.be>
2011-08-22 15:23:09 -0700

    fix syntax + virtualize packages
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,19 +1,24 @@
 class apache::packages {
-	package { apache:
-		ensure => installed,
-		name => $operatingsystem ? {
-			archlinux => "apache",
-			/Debian|Ubuntu/ => "apache2",
-			Centos => "httpd",
-		},
+	@package {
+		'apache':
+			ensure => installed,
+			name => $operatingsystem ? {
+				archlinux => "apache",
+				/Debian|Ubuntu/ => "apache2",
+				Centos => "httpd",
+			};
+
+		'apache-devel':
+			ensure => installed,
+			name => $operatingsystem ? {
+				archlinux => undef,
+				/Debian|Ubuntu/ => 'apache2-threaded-dev',
+				Centos => 'httpd-devel',
+			};
 	}
 
-	package { 'apache-dev':
-		ensure => installed,
-		name => $operatingsystem ? {
-			archlinux => undef,
-			/Debian|Ubuntu/ => 'apache2-threaded-dev',
-			Centos => 'httpd-devel',
-		},
+	realize(Package['apache'])
+	if $devel == 'yes' {
+		realize(Package['apache-devel'])
 	}
 }

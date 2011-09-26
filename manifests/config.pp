147bcff22010324c27543e807f5ab2a401a3e283
Dave Simons <dave@inuits.be>
2011-09-26 14:19:26 +0200

    ensure conf.d dir present, syntax cleanup
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,22 +1,29 @@
 class apache::config {
-	file { 'apache.conf':
-		ensure => present,
-		owner => root,
-		group => root,
-		mode => 0644,
-		name => $operatingsystem ? {
-			archlinux => '/etc/httpd/conf/httpd.conf',
-			/Debian|Ubuntu/ => '/etc/apache2/apache2.conf',
-			Centos => '/etc/httpd/conf/httpd.conf',
-		},
-		content => $operatingsystem ? {
-			archlinux => template('apache/archlinux-apache.conf.erb'),
-			'Centos' => $operatingsystemrelease ? {
-				'6.0' => template('apache/centos6-apache.conf.erb'),
-				'*' => template('apache/centos-apache.conf.erb'),
+	file {
+		"$apache::root/conf.d":
+			ensure => directory,
+			owner => 'root',
+			group => 'root',
+			mode => '0755';
+
+		'apache.conf':
+			ensure => present,
+			owner => root,
+			group => root,
+			mode => 0644,
+			name => $operatingsystem ? {
+				default => '/etc/httpd/conf/httpd.conf',
+				/Debian|Ubuntu/ => '/etc/apache2/apache2.conf',
 			},
-			default => template('apache/debian-apache.conf.erb'),
-		},
-		notify => Service['apache'],
+			content => $operatingsystem ? {
+				archlinux => template('apache/archlinux-apache.conf.erb'),
+				'Centos' => $operatingsystemrelease ? {
+					'6.0' => template('apache/centos6-apache.conf.erb'),
+					'*' => template('apache/centos-apache.conf.erb'),
+				},
+				default => template('apache/debian-apache.conf.erb'),
+			},
+			notify => Service['apache'],
+			require => File["$apache::root/conf.d"];
 	}
 }

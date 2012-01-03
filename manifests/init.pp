5d5538add658d0b6373b892c7b226e5e05331029
Jan Vansteenkiste <jan@vstone.eu>
2012-01-03 15:12:42 +0100

    Removed all nginx modules, split off to own git repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,6 @@ import 'mod/passenger.pp'
 import 'mod/prefork.pp'
 import 'mod/php.pp'
 import 'mod/xsendfile.pp'
-import 'nginx/*'
 
 class apache (
   $apache = $::operatingsystem ? {
@@ -35,12 +34,4 @@ class apache (
   include apache::service
 
   Class['apache::packages'] -> Class['apache::config'] -> Class['apache::service']
-}
-
-class nginx {
-  include nginx::packages
-  include nginx::service
-  include nginx::config
-
-  Class['nginx::packages'] -> Class['nginx::config'] -> Class['nginx::service']
 }

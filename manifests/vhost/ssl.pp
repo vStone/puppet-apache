83c10f8faf5b6a8f91adf387e06d606147778473
Jan Vansteenkiste <jan@vstone.eu>
2012-02-07 15:23:13 +0100

    Initial start for ssl vhosts

---
+++ manifests/vhost/ssl.pp
@@ -0,0 +1,61 @@
+# == Class: apache::vhost::ssl
+#
+# Description of apache::vhost::ssl
+#
+# === Parameters:
+#
+# === Actions:
+#
+# === Requires:
+#
+# === Sample Usage:
+#
+define apache::vhost::ssl (
+  $servername     = undef,
+  $serveraliases  = '',
+  $ensure         = 'present',
+  $ip             = undef,
+  $port           = '443',
+  $admin          = undef,
+  $vhostroot      = undef,
+  $logdir         = undef,
+  $errorlevel     = 'warn',
+  $accesslog      = 'ssl_access.log',
+  $errorlog       = 'ssl_error.log',
+  $docroot        = undef,
+  $docroot_purge  = false,
+  $order          = '10',
+  $vhost_config   = '',
+  $mods           = undef,
+) {
+  ## Copy paste snippets:
+  # template("${module_name}/template.erb")
+  # source => "puppet:///modules/${module_name}/file"
+
+  ##@todo: Require ssl stuff to be present
+
+  ##@todo: include vhost_config
+  $ssl_content = ''
+
+  apache::vhost{$title:
+    ensure        => $ensure,
+    name          => $name,
+    servername    => $servername,
+    serveraliases => $serveraliases,
+    ip            => $ip,
+    port          => $port,
+    admin         => $admin,
+    vhostroot     => $vhostroot,
+    logdir        => $logdir,
+    accesslog     => $accesslog,
+    errorlog      => $errorlog,
+    errorlevel    => $errorlevel,
+    docroot       => $docroot,
+    docroot_purge => $docroot_purge,
+    order         => $order,
+    mods          => $mods,
+    vhost_config  => $ssl_content, ## This is the only thing that differs really.
+  }
+
+}
+

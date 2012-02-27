0e24529c9ac774f064ec1ffd37a7a2552702291c
Jan Vansteenkiste <jan@vstone.eu>
2012-02-27 08:49:44 +0100

    Added more ssl config options

--- manifests/vhost/ssl.pp
+++ manifests/vhost/ssl.pp
@@ -28,6 +28,12 @@
 #                     Filename is relative to the log_dir (apache::vhost).
 #                     Defaults to '' (empty) which is disabled.
 #
+# $ssl_options::
+#
+# $ssl_verify_client::
+#
+# $ssl_verify_depth::
+#
 # === Actions:
 #
 # === Requires:
@@ -37,41 +43,78 @@
 define apache::vhost::ssl (
   $ssl_cert,
   $ssl_key,
-  $ssl_protocol     = 'all -SSLv2',
-  $ssl_ciphersuite  = 'ALL:!ADH:!EXPORT:!SSLv2:RC4+RSA:+HIGH:+MEDIUM:+LOW',
-  $ssl_chain        = undef,
-  $ssl_ca_path      = undef,
-  $ssl_ca_file      = undef,
-  $ssl_ca_crl_path  = undef,
-  $ssl_ca_crl_file  = undef,
-  $ssl_requestlog   = undef,
-  $servername       = undef,
-  $serveraliases    = '',
-  $ensure           = 'present',
-  $ip               = undef,
-  $port             = '443',
-  $admin            = undef,
-  $vhostroot        = undef,
-  $logdir           = undef,
-  $accesslog        = 'access.log',
-  $errorlog         = 'error.log',
-  $errorlevel       = 'warn',
-  $docroot          = undef,
-  $docroot_purge    = false,
-  $dirroot          = undef,
-  $order            = '10',
-  $vhost_config     = '',
-  $mods             = undef
+  $ssl_protocol      = 'all -SSLv2',
+  $ssl_ciphersuite   = 'ALL:!ADH:!EXPORT:!SSLv2:RC4+RSA:+HIGH:+MEDIUM:+LOW',
+  $ssl_chain         = undef,
+  $ssl_ca_path       = undef,
+  $ssl_ca_file       = undef,
+  $ssl_ca_crl_path   = undef,
+  $ssl_ca_crl_file   = undef,
+  $ssl_requestlog    = undef,
+  $ssl_options       = undef,
+  $ssl_verify_client = undef,
+  $ssl_verify_depth  = undef,
+  $servername        = undef,
+  $serveraliases     = '',
+  $ensure            = 'present',
+  $ip                = undef,
+  $port              = '443',
+  $admin             = undef,
+  $vhostroot         = undef,
+  $logdir            = undef,
+  $accesslog         = 'access.log',
+  $errorlog          = 'error.log',
+  $errorlevel        = 'warn',
+  $docroot           = undef,
+  $docroot_purge     = false,
+  $dirroot           = undef,
+  $order             = '10',
+  $vhost_config      = '',
+  $mods              = undef
 ) {
 
-  $server      = $servername ?  { undef => $name, default => $servername,     }
+  ## Same logic as apache::vhost but we redo this since we need $server
+  $server = $servername ? {
+    undef   => $name,
+    default => $servername,
+  }
 
-  $chainfile   = $ssl_chain ?      { undef => '', default => $ssl_chain,      }
-  $ca_path     = $ssl_ca_path?     { undef => '', default => $ssl_ca_path,    }
-  $ca_file     = $ssl_ca_file?     { undef => '', default => $ssl_ca_file,    }
-  $ca_crl_path = $ssl_ca_crl_path? { undef => '', default => $ssl_ca_crl_path,}
-  $ca_crl_file = $ssl_ca_crl_file? { undef => '', default => $ssl_ca_crl_file,}
-  $requestlog  = $ssl_requestlog?  { undef => '', default => $ssl_requestlog, }
+  $chainfile = $ssl_chain ? {
+    undef   => '',
+    default => $ssl_chain,
+  }
+  $ca_path = $ssl_ca_path ? {
+    undef   => '',
+    default => $ssl_ca_path,
+  }
+  $ca_file = $ssl_ca_file ? {
+    undef   => '',
+    default => $ssl_ca_file,
+  }
+  $ca_crl_path = $ssl_ca_crl_path ? {
+    undef   => '',
+    default => $ssl_ca_crl_path,
+  }
+  $ca_crl_file = $ssl_ca_crl_file ? {
+    undef   => '',
+    default => $ssl_ca_crl_file,
+  }
+  $requestlog = $ssl_requestlog ? {
+    undef   => '',
+    default => $ssl_requestlog,
+  }
+  $options = $ssl_options ? {
+    undef   => '',
+    default => $ssl_options,
+  }
+  $verify_client = $ssl_verify_client ? {
+    undef   => '',
+    default => $ssl_verify_client,
+  }
+  $verify_depth = $ssl_verify_depth ? {
+    undef   => '',
+    default => $ssl_verify_depth
+  }
 
   $log_dir = $logdir ? {
     undef   => "${apache::params::vhost_log_dir}/${server}",

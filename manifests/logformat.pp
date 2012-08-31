d4ff45416a4369684ccca38588af647a7f21d705
Jan Vansteenkiste <jan@vstone.eu>
2012-08-31 13:02:21 +0200

    Allow centralized management of logformat statements

---
+++ manifests/logformat.pp
@@ -0,0 +1,43 @@
+# = Definition: apache::logformat
+#
+# Define custom logformats.
+#
+# == Parameters:
+#
+# $format::   The format you want to use. You do NOT need to escape double
+#             double quotes. We will do that for you.
+#
+# $comment::  Additional content that gets added to the logformat definition.
+#
+# $name::     The name of the logformat, you can use this in your vhosts.
+#
+# == Example:
+#
+#   apache::listen { '10.0.0.1_80': }
+#   apache::listen { '80': }
+#
+define apache::logformat (
+  $format,
+  $comment = ''
+) {
+
+  ## Requirements
+  require apache::params
+  require apache::setup::logformat
+
+  ####################################
+  #### Variable checks & Defaults ####
+  ####################################
+
+  ####################################
+  ####       Prepare content      ####
+  ####################################
+
+  $fname = "logformat_${name}"
+
+  apache::confd::file {$fname:
+    confd   => $apache::setup::logformat::confd,
+    content => template('apache/confd/logformat.erb'),
+  }
+
+}

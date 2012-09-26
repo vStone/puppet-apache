628454c9b0a1da15d7adf372849d6208b169d1fd
Jan Vansteenkiste <jan@vstone.eu>
2012-09-26 07:31:41 +0200

    Allow using loadmodule on files other than the default http configuration file

--- manifests/config/loadmodule.pp
+++ manifests/config/loadmodule.pp
@@ -24,15 +24,21 @@ define apache::config::loadmodule (
   $module   = "${name}_module",
   $ensure   = 'present',
   $file     = "mod_${name}.so",
-  $path     = undef
+  $path     = undef,
+  $config   = undef
 ) {
 
   require apache::params
 
+  $config_file = $config ? {
+    undef   => $::apache::params::config_file,
+    default => $config,
+  }
+
   Augeas {
     lens    => 'Httpd.lns',
-    incl    => $::apache::params::config_file,
-    context => "/files${::apache::params::config_file}",
+    incl    => $config_file,
+    context => "/files${config_file}",
   }
 
   $modfile = $path ? {

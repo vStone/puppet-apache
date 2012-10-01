c505540602c5ffcb9c04c0c89c8c5c658dcfa339
Dave Simons <dave@inuits.eu>
2012-10-01 14:58:05 +0200

    add mod_userdir define
    Signed-off-by: Dave Simons <dave@inuits.eu>

---
+++ manifests/vhost/mod/userdir.pp
@@ -0,0 +1,53 @@
+# == Definition: apache::vhost::mod::userdir
+#
+# Setup mod_userdir in a vhost
+#
+# === Parameters:
+#
+# $rewrite_cond:: The rewrite condition to use. Standard apache syntax.
+#                 For now only one string is supported, array support
+#                 may be added at a later time.
+#
+# $rewrite_rule:: The rewrite rule to use when the rewrite_cond matches.
+#                 For now only one string is supported, array support
+#                 may be added at a later time.
+#
+# $vhost::        The name of the vhost to work on. This should be
+#                 identical to the apache::vhost{NAME:} you have setup.
+#
+# $docroot::      Document root.
+#                 Is automaticly filled in if pushed through apache::vhost.
+#
+# $ensure::       If ensure is absent, the configuration file will be
+#                 removed. Defaults to 'present'.
+#
+# $ip::           Ip of the vhost to work on. Should be identical to the
+#                 apache::vhost instance you have setup. Defaults to '*'
+#
+# $port::         Port of the vhost to work on. Should be identical to
+#                 the apache::vhost instance you have setup.
+#                 Defaults to the vhost default.
+#
+define apache::vhost::mod::userdir (
+  $vhost,
+  $ensure        = 'present',
+  $ip            = undef,
+  $port          = undef,
+  $docroot       = undef,
+  $order         = undef,
+  $_automated    = false,
+) {
+
+  $definition = template('apache/vhost/mod/userdir.erb')
+
+  apache::sys::modfile {$title:
+    ensure   => $ensure,
+    vhost    => $vhost,
+    ip       => $ip,
+    port     => $port,
+    content  => $definition,
+    nodepend => $_automated,
+    order    => $order,
+  }
+
+}

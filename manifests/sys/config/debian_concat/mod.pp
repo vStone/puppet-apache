a076e5353db3660ffed53f2539580213c9b84664
Jan Vansteenkiste <jan@vstone.eu>
2012-10-25 15:10:39 +0200

    Initial support for debian style sites-enabled/sites-available configuration

---
+++ manifests/sys/config/debian_concat/mod.pp
@@ -0,0 +1,51 @@
+# == Definition: apache::sys::config::concat::mod
+#
+# Helper file wrapper for extra mods for a certain vhost.
+# This will be added to the vhost configuration using the
+# concat module.
+#
+# === Parameters:
+#
+# $vhost:
+#
+# $ensure:
+#
+#
+# === Todo:
+#
+# TODO: Update documentation
+#
+define apache::sys::config::debian_concat::mod (
+  $vhost,
+  $notify_service = undef,
+  $ip             = undef,
+  $port           = '80',
+  $ensure         = 'present',
+  $content        = '',
+  $nodepend       = false,
+  $order          = undef
+) {
+
+  ## Get the configured configuration style.
+  require apache::params
+  require apache::sys::config::concat::params
+
+  if ($notify_service != undef) and ($nodepend == false) {
+    warning('Setting the notify_service parameter explicitly for a mod will have no impact using the concat configuration style. This should be configured in the vhost or globally.')
+  }
+
+  $fragment_name = "${vhost}_mod_${name}"
+
+  $forder = $order ? {
+    undef   => undef,
+    default => sprintf('%04d',$order),
+  }
+
+  concat::fragment{$fragment_name:
+    ensure  => $ensure,
+    target  => $vhost,
+    content => $content,
+    order   => $forder,
+  }
+
+}

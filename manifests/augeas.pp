5626987a90c0f18136a0669776baac0ee5db86fa
Jan Vansteenkiste <jan@vstone.eu>
2013-01-15 10:57:23 +0100

    Added a new augeas helper that takes ensure => present or absent

---
+++ manifests/augeas.pp
@@ -0,0 +1,25 @@
+define apache::augeas (
+  $value          = undef,
+  $ensure         = 'present',
+  $notify_service = undef,
+  $config         = undef,
+) {
+
+  case $ensure {
+    true, 'present': {
+      apache::augeas::set {$name:
+        value          => $value,
+        config         => $config,
+        notify_service => $notify_service,
+      }
+    }
+    false, 'absent': {
+      apache::augeas::rm {$name:
+        value          => $value,
+        config         => $config,
+        notify_service => $notify_service
+      }
+    }
+  }
+
+}

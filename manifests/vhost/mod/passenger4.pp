cbb28f2176237210202c600215bf9be2ebe252f2
Jan Vansteenkiste <jan@vstone.eu>
2013-07-08 14:23:45 +0200

    Initial support for passenger 4.x

---
+++ manifests/vhost/mod/passenger4.pp
@@ -0,0 +1,89 @@
+# == Definition: apache::vhost::mod::passenger4
+#
+# Enable a passenger configuration in this vhost.
+#
+# === Required Parameters:
+#
+# Your definition should always take the following parameters. When
+# using the mods parameter from vhost, these get set automaticly.
+#
+# $ensure::     Disable or enable this mod. This will/should remove the config
+#               file. Required for apache::sys::modfile.
+#
+# $vhost::      Defined what vhost this module is for.
+#               Required for apache::sys::modfile
+#
+# $ip::         Required for apache::sys::modfile.
+#
+# $port::       Required for apache::sys::modfile.
+#
+# $docroot::    Document root.
+#               Is automaticly filled in if pushed through apache::vhost.
+#
+# === Optional Parameters:
+#
+#
+# === Actions:
+#
+# Creates a apache::sys::modfile for the vhost that has been selected.
+#
+# === Sample Usage:
+#
+# === Todo:
+#
+# TODO: Implement remaining parameters
+# TODO: Update documentation
+#
+define apache::vhost::mod::passenger4 (
+  $vhost,
+  $notify_service       = undef,
+  $ensure               = 'present',
+  $ip                   = undef,
+  $port                 = '80',
+  $docroot              = undef,
+  $order                = undef,
+  $_automated           = false,
+  $_header              = false,
+
+  $comment              = undef,
+  $content              = undef,
+
+  $ruby                 = undef,
+  $app_root             = undef,
+  $spawn_method         = undef,
+  $global_queue         = undef,
+  $passenger_enabled    = undef,
+  $upload_buffer_dir    = undef,
+  $restart_dir          = undef,
+  $friendly_error_pages = undef,
+  $buffer_response      = undef,
+  $user                 = undef,
+  $group                = undef,
+  $min_instances        = undef,
+  $max_requests         = undef,
+  $stat_throttle_rate   = undef,
+  $pre_start            = undef,
+  $high_performance     = undef,
+  $rails_baseuri        = undef,
+  $rails_env            = undef,
+  $rack_baseuri         = undef,
+  $rack_env             = undef
+) {
+
+  require apache::mod::passenger
+
+  ## Generate the content for your module file:
+  $definition = template('apache/vhost/mod/passenger-4.erb')
+
+  apache::sys::modfile {$title:
+    ensure         => $ensure,
+    vhost          => $vhost,
+    ip             => $ip,
+    port           => $port,
+    nodepend       => $_automated,
+    content        => $definition,
+    order          => $order,
+    notify_service => $notify_service,
+  }
+}
+
